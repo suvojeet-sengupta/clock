7169268a7042ec106e963fcbafa229549aa62b60
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2025-12-02 21:09:57 +0530

    Fix: Update compileSdk and targetSdk to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.suvojeet.clock"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.suvojeet.clock"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
